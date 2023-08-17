dabf9a6684e1b3760027dc3a01e600d7f2493ef0
susooo <92291198+susooo@users.noreply.github.com>
2023-08-17 11:17:29 +0900

    installment savings page

--- app/src/main/java/com/example/cashnest/MainActivity.kt
+++ app/src/main/java/com/example/cashnest/MainActivity.kt
@@ -4,10 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.content.Intent
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button1: ImageButton = findViewById(R.id.button1)
+        button1.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+        val button2: ImageButton = findViewById(R.id.button2)
+        button2.setOnClickListener {
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+        }
     }
 }
